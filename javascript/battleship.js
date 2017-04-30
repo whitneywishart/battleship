102922f7849cc51357d1135a0b76ec47ef87fc6a
Whitney <whitneywishart@gmail.com>
2017-04-29 19:15:40 -0700

    add math functions

--- javascript/battleship.js
+++ javascript/battleship.js
@@ -1,13 +1,14 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 var guess;
 var hits = 0;
 var guesses = 0;
 var isSunk = false;
 
 while (isSunk == false) {
-  guess = prompt("Ready, aim, fire! (enter a number between 1 and 6):");
+  guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
   if (guess < 0 || guess > 6) {
     alert ("Please enter a valid cell number");
   } else {
